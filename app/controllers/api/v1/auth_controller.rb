a3248e7cc0ea116bfbfeda6c5dc88b42d5a66e82
Tony Bui <tony.bui.jp@gmail.com>
2022-04-28 00:32:47 +0700

    Login signup rails API

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Api::V1::AuthController < ApplicationController
+  before_action :authorized, only: [:auto_login]
+
+  def login
+    @user = User.find_by(username: params[:username])
+
+    if @user&.authenticate(params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, jwt_token: token }
+    else
+      render json: { error: 'Invalid username or password' }
+    end
+  end
+
+  def auto_login
+    render json: logged_in_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resource :users, only: :create
+      post '/login', to: 'auth#login'
+      get '/auto_login', to: 'auth#auto_login'
+    end
+  end
 end
